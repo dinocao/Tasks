4caad1fbfbdbbd57df2311d7dc9d510cf9c23fb6
cngot <cngot@DinoCao-family>
2016-06-26 21:52:59 +0800

    CalcStats 完成

--- Tasks/src/main/java/CalcStats/CalcStats.java
+++ Tasks/src/main/java/CalcStats/CalcStats.java
@@ -1,9 +1,29 @@
 package CalcStats;
-import java.util.ArrayList;
+
+import java.text.DecimalFormat;
+import java.util.*;
 public class CalcStats {
 
+
 	public static ArrayList take(int[] numberArray) {
-		return null;
+		ArrayList min_max_sequence_average = new ArrayList();
+		Arrays.sort(numberArray);
+		int summember = 0;
+		int minimum = numberArray[0];
+		int maximum = numberArray[numberArray.length - 1];
+		int sequence = numberArray.length;
+		for(int loopNum = 0; loopNum < sequence; loopNum++)
+		{
+			summember += numberArray[loopNum];
+		}
+		DecimalFormat df = new DecimalFormat(".###");
+		min_max_sequence_average = new ArrayList();
+		min_max_sequence_average.add(minimum);
+		min_max_sequence_average.add(maximum);
+		min_max_sequence_average.add(sequence);
+		double average = (double)summember / sequence;
+		min_max_sequence_average.add(df.format((double)average));
+		return min_max_sequence_average;
 	}
 
 }
--- Tasks/src/test/java/CalcStats/CalcStatsTest.java
+++ Tasks/src/test/java/CalcStats/CalcStatsTest.java
@@ -2,15 +2,20 @@ package CalcStats;
 
 import static org.junit.Assert.*;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 
 import org.junit.Test;
 
 public class CalcStatsTest  {
 
+	private DecimalFormat df = new DecimalFormat(".###");
 	@Test
 	public void test() {
-		assertEquals(Arrays.asList(1), CalcStats.take(new int[]{1}));
+		assertEquals(Arrays.asList(1, 1, 1, df.format(1.0)), CalcStats.take(new int[]{1}));
+		assertEquals(Arrays.asList(1, 3, 2, df.format(2.0)), CalcStats.take(new int[]{1,3}));
+		assertEquals(Arrays.asList(1, 5, 5, df.format(2.4)), CalcStats.take(new int[]{1,3,2,1,5}));
+		assertEquals(Arrays.asList(-2, 92, 6, df.format(21.833)), CalcStats.take(new int[]{6, 9, 15, -2, 92, 11}));
 	}
 
 }
